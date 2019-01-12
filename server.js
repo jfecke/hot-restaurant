5f2a8b4515b96f2ac33745b138664595cfea100a
jfecke <jfecke@gmail.com>
2019-01-12 11:13:44 -0600

    finsihed server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Array of Table Objects
+var tables = [];
+var waitlist = [];
+
+
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
@@ -24,3 +29,52 @@ res.sendFile(path.join(__dirname, "tables.html"));
 app.get("/reserve", function(req, res) {
 res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
+
+// Displays a single table, or returns false
+app.get("/api/tables/:table", function(req, res) {
+    let tableid = req.params.uniqueid;
+
+    console.log(chosen);
+  
+    for (var i = 0; i < tables.length; i++) {
+      if (chosen === tables[i].uniqueid) {
+        return res.json(tables[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+
+
+
+
+// Create New Table - takes in JSON input
+app.post("/api/tables", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    let newtable = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html  
+    console.log(newtable);
+    if (tables.length > 5) {
+        waitlist.push(newtable);
+    } else {
+        tables.push(newtable);
+    } 
+    res.json(newtable);
+  });
+  
+  // Starts the server to begin listening
+  // =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
